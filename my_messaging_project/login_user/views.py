48d3f30aa506f8b289d502136689f7f0aa667f38
Khiel Mantilla <khielmantilla@Khiels-MacBook-Pro.local>
2022-10-24 21:04:20 -0400

    first commit

---
+++ my_messaging_project/login_user/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+
+# Create your views here.
+def index(request):
+    return render(request, 'login_user/index.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1'] # hmm
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = UserCreationForm
+    context = { 'form': form }
+    return render(request, 'registration/register.html', context)
